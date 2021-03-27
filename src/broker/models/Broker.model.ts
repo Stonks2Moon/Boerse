fbad7baaed77d5983504b1d0883bfa0022420cd6
Jan Eger <Jan.Eger@Online.de>
2021-03-27 11:58:25 +0100

    added attribute descriptions for Broker Modell

--- src/broker/models/Broker.model.ts
+++ src/broker/models/Broker.model.ts
@@ -1,6 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class BrokerModel {
+  @ApiProperty({
+    description: 'Token of broker, to identify and authenticate.',
+  })
   id: string;
+
+  @ApiProperty({
+    description: 'Type of broker. Important for clearing and access rights',
+    type: 'BrokerType',
+  })
   type: 'private' | 'business' | 'simulation' | 'stockmarket';
+
+  @ApiProperty({
+    description: 'Internal displayName for an specific broker',
+  })
   displayName: string;
+
+  @ApiProperty({
+    description: 'Determines if broker is banned',
+  })
   banned?: boolean;
 }
