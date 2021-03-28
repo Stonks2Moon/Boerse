4fc774208c7ffadff5c3ef57f3bc7f091da9eb72
Timo Scheuermann <timo.scheuermann@gmx.de>
2021-03-28 17:45:08 +0200

    removed app controllers endpoints frm swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,25 @@
 import { Controller, Get, Render } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
+  @ApiExcludeEndpoint(true)
   @Get()
-  @Render('index')
+  @Render('index.hbs')
   renderIndex() {
     // no op
   }
 
+  @ApiExcludeEndpoint(true)
   @Get('pricing')
-  @Render('pricing')
+  @Render('pricing.hbs')
   renderPricing() {
     // no op
   }
 
+  @ApiExcludeEndpoint(true)
   @Get('trading')
-  @Render('trading')
+  @Render('trading.hbs')
   renderTrading() {
     // no op
   }
