afc692387ca509d2cf45eceedcdf213d15e1032b
Jan Eger <Jan.Eger@Online.de>
2021-04-08 11:33:57 +0200

    updated pricing documentation

--- src/pricing/dtos/SetPricing.dto.ts
+++ src/pricing/dtos/SetPricing.dto.ts
@@ -11,6 +11,18 @@ export class SetPricingDto {
   @ApiProperty({
     description: 'Entries for pricing table',
     type: 'PricingEntry',
+    example: [
+      {
+        changeoverLimit: 15000,
+        fixum: 0,
+        tradePromille: 0.35,
+        tradeMin: 21,
+        tradeMax: 52.5,
+        transactionMin: 0.5,
+        transactionMax: 97.5,
+        transactionPromille: 0.65,
+      },
+    ],
   })
   entries: PricingEntry[];
 }
