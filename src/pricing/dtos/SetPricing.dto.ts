b61b34a3c051c3ee8497ca63266f6b09730b7564
Jan Eger <Jan.Eger@Online.de>
2021-04-08 10:48:40 +0200

    Added documentation for pricing module

--- src/pricing/dtos/SetPricing.dto.ts
+++ src/pricing/dtos/SetPricing.dto.ts
@@ -1,6 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { PricingEntry } from '../models/PricingEntry.model';
 
 export class SetPricingDto {
+  @ApiProperty({
+    description: 'Type of broker',
+    example: 'business',
+  })
   type: string;
+
+  @ApiProperty({
+    description: 'Entries for pricing table',
+    type: 'PricingEntry',
+  })
   entries: PricingEntry[];
 }
--- src/pricing/models/PricingEntry.model.ts
+++ src/pricing/models/PricingEntry.model.ts
@@ -1,13 +1,51 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class PricingEntry {
+  @ApiProperty({
+    description: 'Pricing Entry Limit in volume (price*amount)',
+    example: '2500',
+  })
   changeoverLimit: number;
 
+  @ApiProperty({
+    description:
+      'Fixed value (netto) that has to be paid for this pricing entry',
+    example: '0.30',
+  })
   fixum: number;
 
+  @ApiProperty({
+    description: 'Transaction fee in promille',
+    example: '0.75',
+  })
   transactionPromille: number;
+
+  @ApiProperty({
+    description: 'Minimum absolute transaction fee',
+    example: '1.50',
+  })
   transactionMin: number;
+  @ApiProperty({
+    description: 'Maximum absolute transaction fee',
+    example: '1.88',
+  })
   transactionMax: number;
 
+  @ApiProperty({
+    description: 'Trading fee in promille',
+    example: '0.52',
+  })
   tradePromille: number;
+
+  @ApiProperty({
+    description: 'Minimum absolute trading fee',
+    example: '0.50',
+  })
   tradeMin: number;
+
+  @ApiProperty({
+    description: 'Maximum absolute trading fee',
+    example: '1.30',
+  })
   tradeMax: number;
 }
